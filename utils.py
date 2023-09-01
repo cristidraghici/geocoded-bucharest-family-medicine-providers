59df506f7526ca7371d17f5d969bfed63c38a450
cristi <cristi@draghici.net>
2023-09-01 09:09:31 +0300

    optimize the address cleanup function

--- utils.py
+++ utils.py
@@ -9,27 +9,28 @@ def extract_street_name_and_number(address):
     # use alphanumeric characters
     address = unidecode(address)
 
-    # put a comma before the number
-    address = re.compile("nr", flags=re.I).sub(", nr", address)
-
     # ensure we have the proper prefix for the street
-    streetTypeRegexList = [
+    cleanupRegexList = [
+        # fix for number not separated 
+        ["nr", ", nr"],
+
+        # text cleanup (ensure there is a space at the end of the replace string)
+        [r"((?:Numarul\.|Numarul|Nr\.|Nr)\s|(?:Nr\.))", 'Numarul '],
+
         [r"((?:Calea|Cal)\s|(?:Cal\.))", 'Calea'],
-        [r"((?:Piata|Pta)\s|(?:Pta\.))", 'Piata'],
-        [r"((?:Drumul)\s)", 'Drumul'],
-        [r"((?:Strada|Str)\s|(?:Str\.|Stra\.))", 'Strada'],
-        [r"((?:Bulevardul|Bulevard|Bd|Bld|B-dul)\s|(?:Bd\.|Bld\.))", 'Bulevardul'],
-        [r"((?:Soseaua|Sos)\s|(?:Sos\.))", 'Soseaua'],
-        [r"((?:Splaiul|Spl)\s|(?:Spl\.))", 'Splaiul'],
-        [r"((?:Aleea)\s|(?:Al\.))", 'Aleea'],
-        [r"((?:Intarea|Int|Intr)\s|(?:Int\.|Intr\.))", 'Intrarea']
+        [r"((?:Calea|Cal)\s|(?:Cal\.))", 'Calea '],
+        [r"((?:Piata|Pta)\s|(?:Pta\.))", 'Piata '],
+        [r"((?:Drumul)\s)", 'Drumul '],
+        [r"((?:Strada|Str)\s|(?:Str\.|Stra\.))", 'Strada '],
+        [r"((?:Bulevardul|Bulevard|Bd|Bld|B-dul)\s|(?:Bd\.|Bld\.))", 'Bulevardul '],
+        [r"((?:Soseaua|Sos)\s|(?:Sos\.))", 'Soseaua '],
+        [r"((?:Splaiul|Spl)\s|(?:Spl\.))", 'Splaiul '],
+        [r"((?:Aleea)\s|(?:Al\.))", 'Aleea '],
+        [r"((?:Intarea|Int|Intr)\s|(?:Int\.|Intr\.))", 'Intrarea ']
     ]
 
-    for regex in streetTypeRegexList:
+    for regex in cleanupRegexList:
         address = re.sub(regex[0], f'{regex[1]} ', address, flags=re.I)
-
-    # ensure we have the name for the number properly set
-    address = re.sub(r"((?:Numarul|Nr)\s|(?:Nr\.))", 'Numarul ', address, flags=re.I)
 
     # commas and spaces
     while ",," in address:
@@ -37,6 +38,7 @@ def extract_street_name_and_number(address):
     while "  " in address:
         address = re.sub(r"  ", " ", address)
     address = re.sub(r" ,", ",", address)
+    address = re.sub(r", ", ",", address)
     
     # get the street and number
     returnAddress = ''
